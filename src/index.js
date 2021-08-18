319557b26b65fa31fd7b2ced49453b98f3ac24c5
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-08-18 19:38:20 +0300

    Добавляет реализацию таймера
    Добавляет реализацию таймера

--- src/index.js
+++ src/index.js
@@ -1,40 +1,44 @@
 class CountdownTimer {
-  constructor() {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
     this.refs = {
-      days: document.querySelector('.days'),
-      hours: document.querySelector('.hours'),
-      mins: document.querySelector('.mins'),
-      secs: document.querySelector('.secs'),
+      days: document.querySelector('.value[data-value="days"]'),
+      hours: document.querySelector('.value[data-value="hours"]'),
+      mins: document.querySelector('.value[data-value="mins"]'),
+      secs: document.querySelector('.value[data-value="secs"]'),
     };
-    this.id = null;
-    this.targetDate = null;
   }
 
   calc = () => {
     const currentDate = Date.now();
-    const delta = (this.targetDate - currentDate) / 1000;
-    // const days = Math.floor(delta / (1000 * 60 * 60 * 24));
-    // const hours = Math.floor(
-    //   (delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-    // );
-    // const mins = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
-    const secs = Math.floor((delta % (1000 * 60)) / 1000);
-
-    // this.refs.days.textContent = days < 10 ? `0${days}` : days;
-    // this.refs.hours.textContent = hours < 10 ? `0${hours}` : hours;
-    // this.refs.mins.textContent = mins < 10 ? `0${mins}` : mins;
-    this.refs.secs.textContent = secs < 10 ? `0${secs}` : secs;
-    console.log(this.targetDate);
-    console.log(delta);
+    const delta = this.targetDate - currentDate;
+    if (delta < 0) {
+      this.timerStop;
+    } else {
+      const days = Math.floor(delta / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+      );
+      const mins = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
+      const secs = Math.floor((delta % (1000 * 60)) / 1000);
+      this.refs.days.textContent = days < 10 ? `0${days}` : days;
+      this.refs.hours.textContent = hours < 10 ? `0${hours}` : hours;
+      this.refs.mins.textContent = mins < 10 ? `0${mins}` : mins;
+      this.refs.secs.textContent = secs < 10 ? `0${secs}` : secs;
+    }
   };
   timerStart = () => {
-    this.id = setInterval(this.calc, 1000);
+    this.selector = setInterval(this.calc, 1000);
+  };
+  timerStop = () => {
+    clearInterval(this.selector);
   };
 }
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('August 18, 2023 00:00:00').getTime(),
+  targetDate: new Date('Dec 31, 2021').getTime(),
 });
+
 window.addEventListener('DOMContentLoaded', timer.timerStart);
-//console.log(new Date('August 18, 2023 00:00:00').getTime());
